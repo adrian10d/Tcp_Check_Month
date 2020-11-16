e67cbc8307a3870de9ebd7effcffc78381f4e6d4
adrian10d <adrian10d@interia.pl>
2020-11-16 13:21:06 +0100

    Dodanie metody dodawania użytkownika do bazy

--- Server_Support/User.cs
+++ Server_Support/User.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -38,8 +38,8 @@ namespace UsersManager
             return reader.GetRecords<User>().ToList();
         }
 
-        
+
     }
 
-   
+
 }
